f8aef532dbb628e1868f5cb35b50633affc12ca8
Miles Martin <miles@clinton-2.local>
2016-10-11 17:10:42 -0400

    Added a prefab to allow for variable camera zoom in fields within the game.

---
+++ HGD_2016-17/Assets/Scripts/CameraControl/CameraZoomZoneScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoomZoneScript : MonoBehaviour {
+
+	public float cameraSize = 10f;
+	private float defaultCameraSize;
+	private Camera camera;
+	private CameraZoomControlScript cameraControl;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		camera = GameObject.FindWithTag ("MainCamera").GetComponent<Camera> ();
+		defaultCameraSize = camera.orthographicSize;
+		cameraControl = camera.GetComponent<CameraZoomControlScript> ();
+		player = GameObject.FindWithTag ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		if (obj.gameObject == player) {
+			cameraControl.desiredZoomAmount = cameraSize;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D obj) {
+		if (obj.gameObject == player) {
+			cameraControl.desiredZoomAmount = defaultCameraSize;
+		}
+	}
+}
