98e8172602dd8f1fd51b0295c40fb62b072dcbc1
Miles Martin <mrmartin@mtu.edu>
2016-10-23 20:46:24 -0400

    Added level selection screen

---
+++ HGD_2016-17/Assets/Scripts/LevelSelection/LevelSelectionManagerScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelSelectionManagerScript : MonoBehaviour {
+
+    public List<LevelInfo> levels;
+    public int currentPage = 0;
+    public int numberOfPages
+    {
+        get { return levels.Count / 6; }
+    }
+    public List<LevelInfo> currentPageLevels
+    {
+        get
+        {
+            return levels.GetRange(currentPage * 6, 6);
+        }
+    }
+    public bool hasNextPage
+    {
+        get 
+        {
+            return currentPage + 1 < numberOfPages;
+        }
+    }
+    public bool hasPrevPage
+    {
+        get
+        {
+            return currentPage > 0;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        InitLevels();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void InitLevels() {
+        levels = new List<LevelInfo>();
+        levels.Add(new LevelInfo() { name = "1-1", sceneTitle = "MilesTutorial01" });
+        levels.Add(new LevelInfo() { name = "1-2", sceneTitle = "MilesTutorial02" });
+        levels.Add(new LevelInfo() { name = "1-3", sceneTitle = "MilesTutorial03" });
+        levels.Add(new LevelInfo() { name = "1-4", sceneTitle = "MilesTutorial04" });
+        levels.Add(new LevelInfo() { name = "1-5", sceneTitle = "MilesTutorial05" });
+        levels.Add(new LevelInfo() { name = "1-6", sceneTitle = "MilesTutorial06" });
+        levels.Add(new LevelInfo() { name = "2-1", sceneTitle = "MilesTutorial01" });
+        levels.Add(new LevelInfo() { name = "2-2", sceneTitle = "MilesTutorial02" });
+        levels.Add(new LevelInfo() { name = "2-3", sceneTitle = "MilesTutorial03" });
+        levels.Add(new LevelInfo() { name = "2-4", sceneTitle = "MilesTutorial04" });
+        levels.Add(new LevelInfo() { name = "2-5", sceneTitle = "MilesTutorial05" });
+        levels.Add(new LevelInfo() { name = "2-6", sceneTitle = "MilesTutorial06" });
+    }
+}
---
+++ HGD_2016-17/Assets/Scripts/LevelSelection/PageNavigationScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PageNavigationScript : MonoBehaviour {
+
+    private LevelSelectionManagerScript levelManager;
+    public bool isIncrement;
+
+	// Use this for initialization
+	void Start () {
+        levelManager = GameObject.FindWithTag("UIManager").GetComponent<LevelSelectionManagerScript>();
+        GetComponent<Button>().onClick.AddListener(delegate
+        {
+            levelManager.currentPage += isIncrement ? 1 : -1;
+        });
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var color = GetComponent<Image>().color;
+        if (isIncrement)
+        {
+            if (levelManager.hasNextPage)
+            {
+                GetComponent<CanvasGroup>().alpha = 1f;    
+                GetComponent<Button>().enabled = true;
+            }
+            else {
+                GetComponent<CanvasGroup>().alpha = 0.5f;
+                GetComponent<Button>().enabled = false;
+            }
+        }
+        else {
+            if (levelManager.hasPrevPage)
+            {
+                GetComponent<CanvasGroup>().alpha = 1f;
+                GetComponent<Button>().enabled = true;
+            }
+            else {
+                GetComponent<CanvasGroup>().alpha = 0.5f;
+                GetComponent<Button>().enabled = false;
+            }
+        }
+	}
+}
