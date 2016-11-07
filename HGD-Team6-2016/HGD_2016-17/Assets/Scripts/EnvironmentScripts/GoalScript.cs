0f9da79bbbcaff6218f6b23092154e5497bae990
lukedjohnson <ldjohsno@mtu.edu>
2016-11-06 19:39:54 -0500

    update one

--- HGD-Team6-2016/HGD_2016-17/Assets/Scripts/EnvironmentScripts/GoalScript.cs
+++ HGD-Team6-2016/HGD_2016-17/Assets/Scripts/EnvironmentScripts/GoalScript.cs
@@ -1,28 +1,83 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿
+
+using UnityEngine;
 using System.Collections;
+using System.Linq;
+using UnityEngine.Audio;
+public class MixLevel : MonoBehaviour
+{
 
-public class GoalScript : MonoBehaviour {
 
-    private GameObject player;
-    public GameObject uiManager;
-    private UIManagerScript uiManagerScript;
-	public string nextLevelName;
+    public AudioMixer MasterMixer;
 
-    // Use this for initialization
-    void Start () {
-        player = GameObject.FindWithTag("Player");
-        uiManagerScript = uiManager.GetComponent<UIManagerScript>();
+
+    /**
+    *Changes the level of the Music group on the MasterMixer
+    */
+    public void setMudsicLvl(float lvl)
+    {
+
+        MasterMixer.SetFloat("musicLvl", lvl);
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-    void OnCollisionEnter2D (Collision2D obj) {
-        // If the player colllides with the goal, go to the next level
-		if (obj.gameObject == player)
-			SceneManager.LoadScene (nextLevelName);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float current;  //current volume from MasterMixer => Music 
+        float freq;     //current cutoff freqeuncy MasterMixer => Music 
+
+
+        MasterMixer.GetFloat("CutOff_LowPass", out freq); //get current cut off freqeuncy
+        MasterMixer.GetFloat("musicLvl", out current);    //get current volcume
+        //This test if the player is in or out of the gravity sphere 
+        if (!GameObject.FindWithTag("Player").GetComponent<PlayerMovementScript>().influencingSpheres.Any())
+        {
+            //out of gravity sphere 
+            if (current != -8f)
+            {
+                MasterMixer.SetFloat("LowPass_Resonance", 2f);
+                current -= .25f;
+                setMudsicLvl(current);
+            }
+            if (freq > 500)
+            {
+                freq -= 10;
+                MasterMixer.SetFloat("CutOff_LowPass", freq);
+            }
+
+        }
+        else
+        {
+            //in gravity sphere 
+            if (current != 0f)
+            {
+                MasterMixer.SetFloat("LowPass_Resonance", 2f);
+                current += .25f;
+                setMudsicLvl(current);
+
+
+            }
+            if (freq != 5000)
+            {
+                freq += 10;
+                MasterMixer.SetFloat("CutOff_LowPass", freq);
+            }
+            MasterMixer.SetFloat("LowPass_Resonance", 0f);
+
+
+        }
     }
 }
+
+
+
+
+
+
+
+
